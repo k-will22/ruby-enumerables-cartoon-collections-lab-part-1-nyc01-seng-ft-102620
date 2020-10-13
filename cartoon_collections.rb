a20ff66d5ed12bb7e32a93485b45d0dbbab42996
Keith Williams <thestreetkw@hotmail.com>
2020-10-13 23:30:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |element, index| 
-    array[element] + 1 = index
+    (array[element] + 1) = index
    puts index + ". " + element
 end
 end
